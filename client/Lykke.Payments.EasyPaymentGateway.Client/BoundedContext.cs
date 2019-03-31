82c18fbc46949907a31b0609561bbd61e55a5db4
tarurar <tarutin_andrew@mail.ru>
2019-03-31 15:42:04 +0300

    added bounded context name

---
+++ client/Lykke.Payments.EasyPaymentGateway.Client/BoundedContext.cs
@@ -0,0 +1,13 @@
+﻿namespace Lykke.Payments.EasyPaymentGateway.Client
+{
+    /// <summary>
+    /// Easy Payment Gateway bounded context
+    /// </summary>
+    public static class BoundedContext
+    {
+        /// <summary>
+        /// Bounded context name
+        /// </summary>
+        public static string Name = "easy-payment-gateway";
+    }
+}
